1107eb4f1db25df8f3abe5f88c8730d1f301f9a9
Karl Litterfeldt <kalle.litterfeldt@gmail.com>
2018-03-01 13:26:49 +0100

    Updated mix.exs for publishing on hex.pm

--- mix.exs
+++ mix.exs
@@ -2,30 +2,34 @@ defmodule ISOBMFFLang.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :isobmff_lang,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :isobmff_lang,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "ISOBMFF language unpacker",
+      source_url: "https://github.com/Litterfeldt/elixir-isobmff-language-unpacker"
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: []]
+  def application, do: [extra_applications: []]
+
+  defp description() do
+    "Elixir lib that unpacks MP4 and MOV 16 bit language representations into ISO639-2/T language codes."
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Karl Litterfeldt"],
+      licenses: ["MIT License"],
+      links: %{"GitHub" => "https://github.com/Litterfeldt/elixir-isobmff-language-unpacker"}
+    ]
+  end
+
   defp deps, do: []
 end
